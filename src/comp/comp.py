f1f7ab7e88b0e749a3104287d835a0e1aee5ca1e
Carlos Alvarado <Carlosalvaradoarguello10@gmail.com>
2020-06-05 15:23:55 -0500

    added last changes

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -62,9 +62,9 @@ print(f, '(name, age)')
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
 print("All names uppercase:")
-g = [human.name.upper() and human.age + 5 for human in humans]
+g = [Human(human.name.upper(), human.age + 5) for human in humans]
 print(g, 'NAME age + 5')
-
+# Human(human.name.upper(), human.age + 5)
 # Write a list comprehension that contains the square root of all the ages.
 print("Square root of ages:")
 import math
